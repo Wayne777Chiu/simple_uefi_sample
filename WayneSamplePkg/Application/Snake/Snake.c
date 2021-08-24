8f1709e41f285ae0edafcd81a94546cce53c23a5
Wayne.Chiu <Wayne.Chiu@advantech.com.tw>
2021-08-24 18:42:17 +0800

    update the snake application.

---
+++ WayneSamplePkg/Application/Snake/Snake.c
@@ -0,0 +1,13 @@
+/** @file
+  This sample application bases on Sanke PCD setting
+  to print "UEFI Snake play!" to the UEFI Console.
+
+  Copyright (c) 2021 - 2021,  Wayne Chiu. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Uefi.h>
+#include <Library/PcdLib.h>
+#include <Library/Uefilib.h>
+#include <Library/UefiApplicationEntryPoint.h>
