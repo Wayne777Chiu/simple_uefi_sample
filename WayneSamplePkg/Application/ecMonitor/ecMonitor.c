e3cd6bf5e9a07a7c230053d105f1eabf3b7124bb
write.wayne.chiu <write.wayne.chiu@gmail.com>
2022-08-30 17:39:14 +0800

    add application for ec Monitor.

---
+++ WayneSamplePkg/Application/ecMonitor/ecMonitor.c
@@ -0,0 +1,9 @@
+/** @file
+  This sample application shows EC monitor data
+   to the UEFI Console.
+
+  Copyright (c) 2021 - 2022,  Wayne Chiu. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
